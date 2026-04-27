4f3d7155b4a03a0c10c31e4d2eb72e5e9d60c5a0
shreyaa1804 <shreyayadav2222@gmail.com>
2026-04-27 19:17:23 +0530

    updated pet model

--- backend/model/petmodel.js
+++ backend/model/petmodel.js
@@ -1,4 +1,4 @@
-const {Schema,model}= require('../connection');
+const {Schema,model}= require('mongoose');
  const petSchema = new Schema({
     name: { type:String, required:true},
     breed:{type:String},
